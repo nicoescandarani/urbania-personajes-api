9dddc69141aebe3e000cbc0c7a69209c7158d066
Federico Musmanno <federicomusmanno.com>
2021-06-02 22:01:39 -0300

    [UPDATE] urb-connection: test2

--- routes/personajes.js
+++ routes/personajes.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET personajes listing. */
 router.get('/', function(req, res, next) {
   res.send('HOLA!!');
-  console.log('Nico!!');
 });
 
 module.exports = router;
