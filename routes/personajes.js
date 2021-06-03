00dde02c8eefcde47b6f06695c15b7b87456d068
nescandarani <nescandarani@cahttigo.com>
2021-06-02 21:58:05 -0300

    [UPDATE] urb-connection: Test.

--- routes/personajes.js
+++ routes/personajes.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET personajes listing. */
 router.get('/', function(req, res, next) {
   res.send('HOLA!!');
+  console.log('Nico!!');
 });
 
 module.exports = router;
