25e799d263b2484934960396b8574a91c0c39bb0
Federico Musmanno <federicomusmanno.com>
2021-06-06 19:57:58 -0300

    [UPDATE] urb-actions: Joi schema addPersonaje

--- routes/personajes.js
+++ routes/personajes.js
@@ -17,20 +17,22 @@ router.get('/:id', async (req, res) => {
 /* POST Añadir personaje sin ID (el objectID se genera en bbdd). */
 // ! /api/personajes
 router.post('/', async (req, res)=>{
-  let personaje = req.body;
-  personaje = await dataPersonajes.addPersonaje(personaje);
-  res.json(personaje);
-    // const schema = joi.object({
-    //   //VER CAMPOS DEL PERSONAJE PARA VALIDAR
-    // });
-    // const result = schema.validate(req.body);
-    // if(result.error){
-    //   res.status(400).send(result.error.details[0].message);
-    // } else {
-    //   let personaje = req.body;
-    //   personaje = await dataPersonajes.addPersonaje(personaje);
-    //   res.json(personaje);
-    // } 
+    const schema = joi.object({
+      name: joi.string().alphanum().max(20).required(),
+      //VER STATS
+      money: joi.number().min(0).required,
+      level: joi.number().min(1).required,
+      sashaGrade: joi.boolean().required,
+      //VER WEAPONS
+    });
+    const result = schema.validate(req.body);
+    if(result.error){
+      res.status(400).send(result.error.details[0].message);
+    } else {
+      let personaje = req.body;
+      personaje = await dataPersonajes.addPersonaje(personaje);
+      res.json(personaje);
+    } 
 });
 
 router.delete('/:id', async (req, res)=>{
